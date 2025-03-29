d95f1918ffbde656ebcd0d4a09bbe182ef976af1
SzuWei Lin <szuweilin@google.com>
2025-03-29 11:01:50 +0800

    mt6789-common: Support dynamic 64-bit media service
    64-bit GSI requires support both 64_32-bit and 64-bit devices at
    the same time. Add a new variable TARGET_DYNAMIC_64_32_MEDIASERVER
    to put mediaserver_dynamic.rc into GSI. Which starts 64-bit
    mediaserver if ro.zygote is zygote64. Otherwise, it keeps the
    original behavior to start 32-bit meidaserver.
    
    Bug: 236664614
    Test: make gsi_arm64-user; Check system/etc/init
    Test: make gsi_arm-user; Check systen/etc/init
    Test: cts-tradefed; run cts-on-gsi -m CtsViewTestCases
    Change-Id: I74699ec5fafadcbe78a7fef6d10aaed752cd5afa
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,9 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 # Force any prefer32 targets to be compiled as 64 bit.
 IGNORE_PREFER32_ON_DEVICE := true
 
+# Include 64-bit mediaserver to support 64-bit only devices
+TARGET_DYNAMIC_64_32_MEDIASERVER := true
+
 # Include 64-bit drmserver to support 64-bit only devices
 TARGET_DYNAMIC_64_32_DRMSERVER := true
 
