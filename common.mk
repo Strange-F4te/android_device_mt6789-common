ceda5a3a442b2ff2ca2c33ab7886305bf39a1899
Shirayuki39 <lorddemecrius83@proton.me>
2025-06-29 08:27:45 +0900

    mt6789-common: Refactor blob patching
    * Surprise, it turns out some of these aren't getting patched at all if they're declared at two different points
    
    * So we reorganize it and give those that are getting patched at two different times it's own blob section
    
    * Also get rid of libutils-shim as we never needed this at all
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- common.mk
+++ common.mk
@@ -421,14 +421,10 @@ PRODUCT_PACKAGES += \
    libbase_shim \
    libprocessgroup_shim \
    libbinder-v32 \
+   libhidlbase-v32 \
    libstagefright_foundation-v33 \
-   libutils-shim \
-   libutils-v32 \
-   libutils-v32.transsion
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+   libutils_shim \
+   libutils-v32
 
 # Secure Element
 PRODUCT_PACKAGES += \
